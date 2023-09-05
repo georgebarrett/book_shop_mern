fc4c4242f7d80adb3d0e33035ed1909fbf27023a
geab <georgeaustinbarrett@gmail.com>
2023-09-05 11:29:20 +0100

    get route added to make sure all is working so far

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Croeso');
+});
+
 app.listen(5555, () => {
     console.log("5555")
 });
+
