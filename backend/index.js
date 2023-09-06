601736e5c1f5e1a8107c566fd35ec320c4ef8998
geab <georgeaustinbarrett@gmail.com>
2023-09-06 20:31:56 +0100

    books routes moved to their own file in 'routes' folder. index.js refactored

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js"
 
 const app = express();
 
@@ -12,99 +13,7 @@ app.get('/', (request, response) => {
     return response.status(234).send('Croeso');
 });
 
-app.get('/books', async (request, response) => {
-    try {
-        const books = await Book.find({});
-        return response.status(200).json({
-            count: books.length,
-            data: books
-        });    
-    } catch (error) {
-        console.log(error.message)
-        response.status(500).send({ message: error.message });
-    }
-});
-
-app.get('/books/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
-        const book = await Book.findById(id);
-        return response.status(200).json(book);    
-    } catch (error) {
-        console.log(error.message)
-        response.status(500).send({ message: error.message });
-    }
-});
-
-app.post('/books', async (request, response) => {
-    try {
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'send all required fields: title, author, publishYear',
-            });
-        }
-        
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear
-        };
-
-        const book = await Book.create(newBook)
-
-        return response.status(201).send(book); 
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-app.put('/books/:id', async (request, response) => {
-    try {
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'send all required fields: title, author, publishYear',
-            });
-        }
-
-        const { id } = request.params;
-        const result = await Book.findByIdAndUpdate(id, request.body);
-
-        if (!result) {
-            return response.status(404).json({ message: 'Book not found.' });
-        }
-        return response.status(200).send({ message: 'Book data updated.' });
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message })
-    }
-});
-
-app.delete('/books/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
-        const result = await Book.findByIdAndDelete(id);
-
-        if (!result) {
-            return response.status(404).json({ message: 'Book not found.' })
-        }
-        return response.status(200).send({ message: 'Book has been deleted.' })
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
+app.use('/books', booksRoute)
 
 mongoose
     .connect(mongoDBURL)
