db4900a648f9dcf236529933e8be1a8803119836
geab <georgeaustinbarrett@gmail.com>
2023-09-06 19:25:35 +0100

    get request made so book data can be obtained using its id

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,18 @@ app.get('/books', async (request, response) => {
         console.log(error.message)
         response.status(500).send({ message: error.message });
     }
-})
+});
+
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book);    
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({ message: error.message });
+    }
+});
 
 app.post('/books', async (request, response) => {
     try {
