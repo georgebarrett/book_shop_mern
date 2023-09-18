9aaeb9c5efd86707edda71620d714f06f83e48a1
geab <georgeaustinbarrett@gmail.com>
2023-09-18 14:45:23 +0100

    modules for Home page imported

--- book_shop/src/components/Home.jsx
+++ book_shop/src/components/Home.jsx
@@ -1,4 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Spinner from './Spinner';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+
+
 
 const Home = () => {
   return (
