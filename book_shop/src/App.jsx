75d475e2d39e719f92c2cb4f5a2dc5af4237095c
geab <georgeaustinbarrett@gmail.com>
2023-09-18 12:57:58 +0100

    components structure created. components imported into app.jsx. Routes and Route used as wrappers for navigating through the components

--- book_shop/src/App.jsx
+++ book_shop/src/App.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import CreateBook from "./components/CreateBook";
+import ShowBook from "./components/ShowBook";
+import EditBook from "./components/EditBook";
+import DeleteBook from "./components/DeleteBook";
+
 
 const app = () => {
   return (
-    <div className="bg-red-400 text-white">BOOKS</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="books/delete/:id" element={<DeleteBook />} />
+    </Routes>
+
   )
 };
 
